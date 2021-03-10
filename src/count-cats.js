d9a4afaf14dca5596b898d522bc1be9c3974e643
Maxim Bordea <maxbordea@gmail.com>
2021-03-10 22:35:33 +0200

    feat: implement function count-cats and dream-team

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(backyard) {
+  
   // remove line with error and write your code here
+  let counter = 0;
+
+  for (let i = 0; i < backyard.length; i++) {
+    for (let j = 0; j < backyard[i].length; j++) {
+    backyard[i][j] === '^^' ? counter++ : counter;
+  }
+}
+  return counter;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
   // remove line with error and write your code here
+  if (!Array.isArray(members)) return false;
+  return members
+    .filter((element) => typeof element === "string")
+    .map((str) => str.trim().charAt(0).toUpperCase())
+    .sort()
+    .join("");
 };
