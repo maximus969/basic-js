cb762942526080f538383c51e74f38171a300af5
Maxim Bordea <maxbordea@gmail.com>
2021-03-11 13:10:06 +0200

    feat: implement logic of carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,16 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
+  //  throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if (typeof sampleActivity !== 'string' || Number.isNaN(parseInt(sampleActivity))) {
+    return false
+  } else if (parseInt(sampleActivity) <= 0 || parseInt(sampleActivity) >= 15) {
+    return false
+  } else { 
+  let age;
+  age = (HALF_LIFE_PERIOD / 0.693) * Math.abs(Math.log(sampleActivity / MODERN_ACTIVITY));
+  return Math.ceil(age);
+  }
 };
