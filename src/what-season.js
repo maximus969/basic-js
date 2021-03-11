a80cd74f7d8a4279ebce002394ab8fec58f0e4bf
Maxim Bordea <maxbordea@gmail.com>
2021-03-11 11:03:04 +0200

    feat: implement logic what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
+module.exports = function getSeason(date) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if (typeof date === "undefined") {
+    return "Unable to determine the time of year!";
+  } else if (isNaN(date.getTime())) {
+    throw  'Error'
+  } else if (date.getMonth() < 2 || date.getMonth() === 11) {
+    return "winter";
+  } else if (date.getMonth() < 5) {
+    return "spring";
+  } else if (date.getMonth() < 8) {
+    return "summer";
+  } else {
+    return "autumn";
+  }
 };
