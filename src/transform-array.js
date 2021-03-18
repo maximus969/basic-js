7bd830f6a1568de210e03488ee3208781ae6a470
Maxim Bordea <maxbordea@gmail.com>
2021-03-18 23:59:08 +0200

    feat: implement logic of transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+module.exports = function transform(arr) {
+  //  throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if (!Array.isArray(arr)) throw new Error();
+
+  let result = [];
+  let controls = [
+    "--discard-next",
+    "--discard-prev",
+    "--double-next",
+    "--double-prev",
+  ];
+  for (let i = 0; i < arr.length; i++) {
+    if (!controls.includes(arr[i])) {
+      result.push(arr[i]);
+    } else if (arr[i] === "--double-next" && i < arr.length - 1) {
+      result.push(arr[i + 1]);
+    } else if (arr[i] === "--double-prev" && i > 0 && arr[i - 2] !== "--discard-next") {
+      result.push(arr[i - 1]);
+    } else if (arr[i] === "--discard-prev" && arr[i - 2] !== "--discard-next") {
+      result.pop();
+    } else if (arr[i] === "--discard-next") {
+      i++;
+    }
+  }
+  return result;
 };
