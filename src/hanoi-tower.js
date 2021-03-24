7f03eb7704ecab502a58bf75351bdc1011b9f0e7
Maxim Bordea <maxbordea@gmail.com>
2021-03-24 12:45:16 +0200

    feat: implement logic of hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let hanoi = {};
+  let turns;
+
+  for (let i = 0; i <= disksNumber; i++) {
+    turns = 2 ** i;
+  }
+  turns = turns - 1;
+
+  let turnsPerSecond = (turnsSpeed / 60) / 60;
+  let seconds = Math.floor(turns / turnsPerSecond);
+
+  hanoi['turns'] = turns;
+  hanoi['seconds'] = seconds;
+  return hanoi;
 };
