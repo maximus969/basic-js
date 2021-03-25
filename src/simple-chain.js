8b70b40a4486105bb1de3a565bbf1c15992282ef
Maxim Bordea <maxbordea@gmail.com>
2021-03-25 23:41:43 +0200

    feat: implement logic simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    return {...this, chain: [...this.chain, value !== 'undefined' ? value : '']}
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
-  },
+    if (position < 1 || position > this.chain.length) throw Error
+    return {...this, chain: [...this.chain].filter((_, index) => index !== position - 1 )}
+},
   reverseChain() {
-    throw new CustomError('Not implemented');
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    return {...this, chain: [...this.chain].reverse()}
   },
   finishChain() {
-    throw new CustomError('Not implemented');
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    return this.chain.map(el => `( ${el} )`).join('~~');
   }
 };
 
