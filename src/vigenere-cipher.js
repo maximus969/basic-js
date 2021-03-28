46c87078c14b5c4707893073ae8abf43040a5bef
Maxim Bordea <maxbordea@gmail.com>
2021-03-28 11:11:47 +0300

    feat: implement logic vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct) {
+    this.reverse = direct === undefined ? false : !direct;
+  }
+
+  encode(textLetter, secretLetter, encrypt) {
+    const resultCode =
+      ([textLetter, secretLetter]
+        .map((letter) => letter.charCodeAt() - 65)
+        .reduce((a, b) => (encrypt ? a + b : a - b + 26)) %
+        26) +
+      65;
+    return String.fromCharCode(resultCode);
+  }
+
+  getResult(text, secret, encrypt) {
+    if (text === undefined || secret === undefined) throw Error;
+
+    const secretLen = secret.length;
+
+    let result = text
+      .toUpperCase()
+      .split("")
+      .reduce(
+        ({ str, index }, current) => {
+          if (current.match(/[A-Z]/) === null) {
+            return { str: str + current, index };
+          } else {
+            const letterEncoded = this.encode(
+              current,
+              secret.toUpperCase()[index % secretLen],
+              encrypt
+            );
+            return { str: str + letterEncoded, index: index + 1 };
+          }
+        },
+        { str: "", index: 0 }
+      ).str;
+    if (this.reverse) {
+      result = result.split("").reverse().join("");
+    }
+    return result;
+  }
+
+  encrypt(text, secret) {
+    return this.getResult(text, secret, true);
+  }
+  decrypt(text, secret) {
+    return this.getResult(text, secret, false);
   }
 }
 
